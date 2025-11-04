ef1c5edfd80ace0c2bf2e7e17f035eb7689cf7d1
joshcesana <joshuacesana@gmail.com>
2025-11-03 20:18:46 -0500

    Update navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,67 +1,25 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    {
-      text: 'Home',
-      href: getPermalink('/'),
-    },
-    {
-      text: 'Services',
-      href: '/#services',
-    },
-    {
-      text: 'About',
-      href: getPermalink('/about'),
-    },
-    {
-      text: 'FAQ',
-      href: '/#faq',
-    }
+    { text: 'Home', href: getPermalink('/') },
+    { text: 'Services', href: '/#services' },
+    { text: 'Pricing', href: '/#pricing' },
+    { text: 'About', href: getPermalink('/about') },
+    { text: 'FAQ', href: '/#faq' },
   ],
-  actions: [{ text: 'Contact', href: '#contact' }],
+  ctaLabel: 'Fix My Books Now',
+  ctaModal: '#contact-modal',
 };
 
 export const footerData = {
   links: [
-    // {
-    //   title: 'Product',
-    //   links: [
-    //     { text: 'Features', href: '#' },
-    //     { text: 'Security', href: '#' },
-    //     { text: 'Team', href: '#' },
-    //     { text: 'Enterprise', href: '#' },
-    //     { text: 'Customer stories', href: '#' },
-    //     { text: 'Pricing', href: '#' },
-    //     { text: 'Resources', href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: 'Platform',
-    //   links: [
-    //     { text: 'Developer API', href: '#' },
-    //     { text: 'Partners', href: '#' },
-    //     { text: 'Atom', href: '#' },
-    //     { text: 'Electron', href: '#' },
-    //     { text: 'AstroWind Desktop', href: '#' },
-    //   ],
-    // },
-    // {
-    //   title: 'Support',
-    //   links: [
-    //     { text: 'Docs', href: '#' },
-    //     { text: 'Community Forum', href: '#' },
-    //     { text: 'Professional Services', href: '#' },
-    //     { text: 'Skills', href: '#' },
-    //     { text: 'Status', href: '#' },
-    //   ],
-    // },
     {
       links: [
         { text: 'Services', href: '/#services' },
+        { text: 'Pricing', href: '/#pricing' },
         { text: 'About', href: '/about' },
         { text: 'FAQ', href: '/#faq' },
-        { text: 'Contact', href: '/#contact' },
       ],
     },
   ],
@@ -69,12 +27,5 @@ export const footerData = {
     { text: 'Disclaimer', href: getPermalink('/disclaimer') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  // socialLinks: [
-  //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-  //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-  //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  //   // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-  //   // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
-  // ],
   footNote: `&copy; ${new Date().getFullYear()} Rock Bookkeeping. All rights reserved.`,
 };
