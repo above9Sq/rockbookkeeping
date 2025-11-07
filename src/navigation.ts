26d65c238942719b94cde236cb56090c9a9516c5
joshcesana <joshuacesana@gmail.com>
2025-11-07 13:38:06 -0500

    Update footer heart emoji from red to yellow

--- src/navigation.ts
+++ src/navigation.ts
@@ -27,5 +27,5 @@ export const footerData = {
     { text: 'Disclaimer', href: getPermalink('/disclaimer') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  footNote: `&copy; ${new Date().getFullYear()} Rock Bookkeeping. Made with <span class="text-red-400">❤️</span> in Pittsburgh.<br class="sm:hidden"> All&nbsp;rights&nbsp;reserved.`,
+  footNote: `&copy; ${new Date().getFullYear()} Rock Bookkeeping. Made with <span class="text-red-400">💛</span> in Pittsburgh.<br class="sm:hidden"> All&nbsp;rights&nbsp;reserved.`,
 };
