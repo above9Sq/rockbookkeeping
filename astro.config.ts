fa783782b4cc914949681a5575882a0facd438cc
joshcesana <joshuacesana@gmail.com>
2025-11-16 22:39:14 -0500

    Fix render blocking css file

--- astro.config.ts
+++ astro.config.ts
@@ -25,6 +25,10 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
 
+  build: {
+    inlineStylesheets: 'auto',
+  },
+
   integrations: [
     sitemap(),
     mdx(),
